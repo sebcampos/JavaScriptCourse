421a22d3c784e1e75edfbe28f24e8cf0084c1a4d
sebash <sebcampos@criptext.com>
2022-08-23 19:52:06 -0700

    completed first assignment, home page and coming soon pages

--- DevServer/JsDev/urls.py
+++ DevServer/JsDev/urls.py
@@ -4,4 +4,11 @@ from . import views
 urlpatterns = \
 [
     path("", views.landing_page, name="landing_page"),
+    path("hw2", views.hw2, name="hw2"),
+    path("hw3", views.hw3, name="hw3"),
+    path("hw4", views.hw4, name="hw4"),
+    path("hw5", views.hw5, name="hw5"),
+    path("hw6", views.hw6, name="hw6"),
+    path("hw7", views.hw7, name="hw7"),
+    path("hw8", views.hw8, name="hw8"),
 ]
--- DevServer/JsDev/views.py
+++ DevServer/JsDev/views.py
@@ -6,4 +6,39 @@ from django.core.handlers.wsgi import WSGIRequest
 
 @require_GET
 def landing_page(request: WSGIRequest) -> HttpResponse:
-    return render(request, 'landing_page.html', context={})
+    return render(request, 'index.html', context={})
+
+
+@require_GET
+def hw2(request: WSGIRequest) -> HttpResponse:
+    return render(request, 'hw2.html', context={})
+
+
+@require_GET
+def hw3(request: WSGIRequest) -> HttpResponse:
+    return render(request, 'hw3.html', context={})
+
+
+@require_GET
+def hw4(request: WSGIRequest) -> HttpResponse:
+    return render(request, 'hw4.html', context={})
+
+
+@require_GET
+def hw5(request: WSGIRequest) -> HttpResponse:
+    return render(request, 'hw5.html', context={})
+
+
+@require_GET
+def hw6(request: WSGIRequest) -> HttpResponse:
+    return render(request, 'hw6.html', context={})
+
+
+@require_GET
+def hw7(request: WSGIRequest) -> HttpResponse:
+    return render(request, 'hw7.html', context={})
+
+
+@require_GET
+def hw8(request: WSGIRequest) -> HttpResponse:
+    return render(request, 'hw8.html', context={})
